bf1e5999507dcca106ed344cf573d3e91330450f
csmilne <35082425+csmilne@users.noreply.github.com>
2020-10-18 12:52:13 -0400

    Ch4()

--- app.js
+++ app.js
@@ -1,24 +1,26 @@
-var express = require('express');
-var chalk = require('chalk');
-var debug = require('debug')('app');
-var morgan = require('morgan');
-var path = require('path');
-const { expr } = require('jquery');
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
+const port = process.env.port || 3000;
 
-var app = express();
+//const { expr } = require('jquery');
+
+const app = express();
 
 app.use(morgan('tiny'));
-app.use('/css', express.static(path.join(__dirname,'/node_modules/bootstrap/dist/css')));
-app.use('/js', express.static(path.join(__dirname,'/node_modules/bootstrap/dist/js')));
-app.use('/js', express.static(path.join(__dirname,'/node_modules/jquery/dist')));
-
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
 app.use(express.static(path.join(__dirname, '/public/')));
-app.get('/', function(req, res){
+
+app.get('/', (req, res) => {
     res.sendfile(path.join(__dirname, '/Views/index.html'));
 });
 
-app.listen(3000, function(){
+app.listen(port, () => {
     //to run debug at cmd enter: set DEBUG=* & node app.js
-    //to run debug at cmd enter: set DEBUG=app & node app.js
-    debug(`listening on port ${chalk.green('3000')}`);
+    //to run debug at cmd enter: set DEBUG=*app & node app.js
+    debug(`listening on port ${chalk.green(port)}`);
 });
